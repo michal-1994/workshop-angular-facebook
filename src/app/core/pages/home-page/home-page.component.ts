cdcf0bffcae95d0a413f129657de3254d42dff2d
michal <michalgrzegorczyk04@gmail.com>
2022-06-09 09:50:27 +0200

    Add posts from json

--- src/app/core/pages/home-page/home-page.component.ts
+++ src/app/core/pages/home-page/home-page.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import fixturePosts from '../../../../fixtures/posts.fixture.json';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
-
+    fakePosts = fixturePosts;
+    
   constructor() { }
 
   ngOnInit(): void {
